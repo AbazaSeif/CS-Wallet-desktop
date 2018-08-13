efe2230303969a4d5518ae85849a687e0fa97491
IGN <mcshe@DESKTOP-SQT517A>
2018-08-13 13:20:04 +0300

    Commit 88. Upgrading FormUtils

--- src/main/java/com/credits/wallet/desktop/utils/FormUtils.java
+++ src/main/java/com/credits/wallet/desktop/utils/FormUtils.java
@@ -1,6 +1,7 @@
 package com.credits.wallet.desktop.utils;
 
 import com.credits.wallet.desktop.thread.GetBalanceUpdater;
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.StageStyle;
@@ -28,8 +29,7 @@ public class FormUtils {
 
     public static void displayBalance(String coin, Label label) {
         label.setText(MSG_RETRIEVE_BALANCE);
-        Thread getBalanceThread = new Thread(new GetBalanceUpdater(coin, label));
-        getBalanceThread.start();
+        Platform.runLater(new GetBalanceUpdater(coin, label));
     }
 }
 
