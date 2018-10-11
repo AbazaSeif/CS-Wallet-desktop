ac20a76fdf25c7c18874d964b5781d0b060b6006
IGN <mcshe@DESKTOP-SQT517A>
2018-10-11 13:08:19 +0300

    Commit 321. Adding thread

--- src/main/java/com/credits/wallet/desktop/utils/FormUtils.java
+++ src/main/java/com/credits/wallet/desktop/utils/FormUtils.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.StageStyle;
 
+/**
+ * Created by goncharov-eg on 26.01.2018.
+ */
 public class FormUtils {
     private static final String MSG_RETRIEVE_BALANCE = "Retrieving balance...";
 
@@ -29,7 +32,7 @@ public class FormUtils {
 
     public static void displayBalance(String coin, Label label) {
         label.setText(MSG_RETRIEVE_BALANCE);
-        Platform.runLater(new GetBalanceUpdater(coin, label));
+        new Thread(new GetBalanceUpdater(coin, label)).start();
     }
 }
 
